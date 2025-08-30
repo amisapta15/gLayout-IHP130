1fa5c2f044743f357fb6433360cba04d5b40885a
Saptarshi Ghosh <sapta15@gmail.com>
2025-08-30 22:30:42 +0200

    layout up

--- blocks/composite/dpi_adexp_neuron/design_data/des_py/trimmer.py
+++ blocks/composite/dpi_adexp_neuron/design_data/des_py/trimmer.py
@@ -46,25 +46,18 @@ from current_mirror import current_mirror,add_cm_labels
 
 def trimmer(
         pdk: MappedPDK,
-        #device_type: str = "nmos", 
-        #placement: str = "horizontal",
-        #width: tuple[float,float] = (3,3),
-        #length: tuple[float,float] = (None,None),
-        #fingers: tuple[int,int] = (1,1),
-        #multipliers: tuple[int,int] = (1,1),
-        #with_substrate_tap: bool = False,
-        #dummy_1: tuple[bool,bool] = (True,True),
-        #dummy_2: tuple[bool,bool] = (True,True),
-        #tie_layers1: tuple[str,str] = ("met2","met1"),
-        #tie_layers2: tuple[str,str] = ("met2","met1"),
-        #sd_rmult: int=1,
-        #**kwargs
+        multipliers: tuple[int,int] = (1,1),
+        dummy_1: tuple[bool,bool] = (True,True),
+        dummy_2: tuple[bool,bool] = (True,True),
+        tie_layers1: tuple[str,str] = ("met2","met1"),
+        tie_layers2: tuple[str,str] = ("met2","met1"),
         ) -> Component:
 
     pdk.activate()
     
     #top level component
     top_level = Component(name="trimmer")
+    viam2m3 = via_stack(pdk, "met2", "met3", centered=True) #met2 is the bottom layer. met3 is the top layer.
 
     mp1_2=resistor(pdk,width=0.28,length=8.0,num_series=2)
     mp1_2_ref=prec_ref_center(mp1_2)
@@ -74,6 +67,13 @@ def trimmer(
     pfet_3 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=2, with_dummy=True, dnwell=False,  with_substrate_tap=False)
     pfet_4 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=1, with_dummy=True, dnwell=False,  with_substrate_tap=False)
     pfet_5 = pmos(pdk, length=8.0,width=0.28, fingers=1, multipliers=1, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+
+
+    mp19_ref = prec_ref_center(pfet_4)
+    top_level.add(mp19_ref)
+    top_level.add(mp1_2_ref)
+
+    
     
     mp3_ref = prec_ref_center(pfet_1)
     mp5_ref = prec_ref_center(pfet_2) 
@@ -111,15 +111,15 @@ def trimmer(
     top_level.add(mp6_ref)
     top_level.add(mp8_ref)
     top_level.add(mp11_ref)
-    top_level.add(mp1_2_ref)
-
     
 
     
+
     
-    ncm12 = current_mirror(pdk,numcols=1,width=0.28,length=8.0,device='nfet',with_tie=False)
+    
+    ncm12 = current_mirror(pdk,numcols=1,width=0.28,length=8.0,device='nfet',with_tie=True)
     ncm12_ref= prec_ref_center(ncm12)
-    ncm34 = current_mirror(pdk,numcols=1,width=0.28,length=8.0,device='nfet',with_tie=False)
+    ncm34 = current_mirror(pdk,numcols=1,width=0.28,length=8.0,device='nfet',with_tie=True)
     ncm34_ref= prec_ref_center(ncm34)
     
 
@@ -133,7 +133,7 @@ def trimmer(
     top_level.add(ncm12_ref)
     top_level.add(ncm34_ref)
 
-    mp19_ref = prec_ref_center(pfet_4)
+    
     mp20_ref = prec_ref_center(pfet_4)
     mp18_ref = prec_ref_center(pfet_3)
     mp17_ref = prec_ref_center(pfet_3)
@@ -143,8 +143,8 @@ def trimmer(
     mp19_ref.movex(top_level.xmin + (evaluate_bbox(pfet_4)[1]/2) + 2* pdk.util_max_metal_seperation())
     mp19_ref.movey(top_level.ymax + (2* evaluate_bbox(pfet_4)[1]/2) - 2* pdk.util_max_metal_seperation())
 
-    mp20_ref.movex(top_level.xmin + (4* evaluate_bbox(pfet_4)[1]/2) + 4* pdk.util_max_metal_seperation())
-    mp20_ref.movey(top_level.ymax + (2* evaluate_bbox(pfet_4)[1]/2) - 2* pdk.util_max_metal_seperation())
+    mp20_ref.movex(top_level.xmin + (5* evaluate_bbox(pfet_4)[1]/2) + 5* pdk.util_max_metal_seperation())
+    mp20_ref.movey(top_level.ymax + ( evaluate_bbox(pfet_4)[1]/2) +  pdk.util_max_metal_seperation())
 
     mp18_ref.movex(top_level.xmin + (4* evaluate_bbox(pfet_3)[1]/2) + 4*pdk.util_max_metal_seperation())
     mp18_ref.movey(top_level.ymax + (2* evaluate_bbox(pfet_3)[1]/2) -  pdk.util_max_metal_seperation())
@@ -158,66 +158,67 @@ def trimmer(
     mp12_ref.movex(top_level.xmin + (10* evaluate_bbox(pfet_3)[1]/2) + 10* pdk.util_max_metal_seperation())
     mp12_ref.movey(top_level.ymax + (2* evaluate_bbox(pfet_3)[1]/2) -  pdk.util_max_metal_seperation())
 
-    ############################
-    
-    pfet_4 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=4, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    pfet_5 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=4, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    mp15_ref=prec_ref_center(pfet_3)
-    mp13_ref=prec_ref_center(pfet_5)
-    mp14_ref=prec_ref_center(pfet_4)
 
-    mp13_ref.movex(top_level.xmax + (evaluate_bbox(pfet_5)[0]/2) + pdk.util_max_metal_seperation())
-    mp15_ref.movex(top_level.xmax + (evaluate_bbox(pfet_4)[0]/2) + pdk.util_max_metal_seperation())
-    mp14_ref.movex(top_level.xmax + (evaluate_bbox(pfet_4)[0]/2) + pdk.util_max_metal_seperation())
-    top_level.add(mp15_ref)
+    # ############################
     
-    
-    mp15_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
-    mp14_ref.movey(top_level.ymin + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
+    # pfet_4 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=4, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # pfet_5 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=4, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # mp15_ref=prec_ref_center(pfet_3)
+    # mp13_ref=prec_ref_center(pfet_5)
+    # mp14_ref=prec_ref_center(pfet_4)
 
-    ############################
-    top_level.add(mp13_ref)
-    top_level.add(mp14_ref)
-    ############################
-    pfet_6 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=8, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    pfet_7 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=8, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    mp23_ref=prec_ref_center(pfet_3)
-    mp21_ref=prec_ref_center(pfet_7)
-    mp22_ref=prec_ref_center(pfet_6)
-
-    mp23_ref.movex(top_level.xmax + (evaluate_bbox(pfet_3)[0]/2) + pdk.util_max_metal_seperation())
-    mp21_ref.movex(top_level.xmax + (evaluate_bbox(pfet_7)[0]/2) + pdk.util_max_metal_seperation())
-    mp22_ref.movex(top_level.xmax + (evaluate_bbox(pfet_6)[0]/2) + pdk.util_max_metal_seperation())
-    top_level.add(mp21_ref)
+    # mp13_ref.movex(top_level.xmax + (evaluate_bbox(pfet_5)[0]/2) + pdk.util_max_metal_seperation())
+    # mp15_ref.movex(top_level.xmax + (evaluate_bbox(pfet_4)[0]/2) + pdk.util_max_metal_seperation())
+    # mp14_ref.movex(top_level.xmax + (evaluate_bbox(pfet_4)[0]/2) + pdk.util_max_metal_seperation())
+    # top_level.add(mp15_ref)
     
     
-    mp23_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
-    mp22_ref.movey(top_level.ymin - 2* (evaluate_bbox(pfet_6)[1]/2) - 2* pdk.util_max_metal_seperation())
+    # mp15_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
+    # mp14_ref.movey(top_level.ymin + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
 
-    ############################
-    top_level.add(mp22_ref)
-    top_level.add(mp23_ref)
-    ###########################
+    # ############################
+    # top_level.add(mp13_ref)
+    # top_level.add(mp14_ref)
+    # ############################
+    # pfet_6 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=8, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # pfet_7 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=8, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # mp23_ref=prec_ref_center(pfet_3)
+    # mp21_ref=prec_ref_center(pfet_7)
+    # mp22_ref=prec_ref_center(pfet_6)
 
-    pfet_8 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=16, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    pfet_9 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=16, with_dummy=True, dnwell=False,  with_substrate_tap=False)
-    mp26_ref=prec_ref_center(pfet_3)
-    mp24_ref=prec_ref_center(pfet_9)
-    mp25_ref=prec_ref_center(pfet_8)
-
-    mp26_ref.movex(top_level.xmax + (evaluate_bbox(pfet_3)[0]/2) + pdk.util_max_metal_seperation())
-    mp24_ref.movex(top_level.xmax + (evaluate_bbox(pfet_9)[0]/2) + pdk.util_max_metal_seperation())
-    mp25_ref.movex(top_level.xmax + (evaluate_bbox(pfet_8)[0]/2) + pdk.util_max_metal_seperation())
-    top_level.add(mp24_ref)
+    # mp23_ref.movex(top_level.xmax + (evaluate_bbox(pfet_3)[0]/2) + pdk.util_max_metal_seperation())
+    # mp21_ref.movex(top_level.xmax + (evaluate_bbox(pfet_7)[0]/2) + pdk.util_max_metal_seperation())
+    # mp22_ref.movex(top_level.xmax + (evaluate_bbox(pfet_6)[0]/2) + pdk.util_max_metal_seperation())
+    # top_level.add(mp21_ref)
     
     
-    mp26_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
-    mp25_ref.movey(top_level.ymin - (2* evaluate_bbox(pfet_8)[1]/2) - 2* pdk.util_max_metal_seperation())
+    # mp23_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
+    # mp22_ref.movey(top_level.ymin - 2* (evaluate_bbox(pfet_6)[1]/2) - 2* pdk.util_max_metal_seperation())
 
-    ############################
-    top_level.add(mp26_ref)
-    top_level.add(mp25_ref)
-    ############################
+    # ############################
+    # top_level.add(mp22_ref)
+    # top_level.add(mp23_ref)
+    # ###########################
+
+    # pfet_8 = pmos(pdk, length=0.28,width=2.0, fingers=1, multipliers=16, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # pfet_9 = pmos(pdk, length=2.0,width=2.0, fingers=1, multipliers=16, with_dummy=True, dnwell=False,  with_substrate_tap=False)
+    # mp26_ref=prec_ref_center(pfet_3)
+    # mp24_ref=prec_ref_center(pfet_9)
+    # mp25_ref=prec_ref_center(pfet_8)
+
+    # mp26_ref.movex(top_level.xmax + (evaluate_bbox(pfet_3)[0]/2) + pdk.util_max_metal_seperation())
+    # mp24_ref.movex(top_level.xmax + (evaluate_bbox(pfet_9)[0]/2) + pdk.util_max_metal_seperation())
+    # mp25_ref.movex(top_level.xmax + (evaluate_bbox(pfet_8)[0]/2) + pdk.util_max_metal_seperation())
+    # top_level.add(mp24_ref)
+    
+    
+    # mp26_ref.movey(top_level.ymax + (evaluate_bbox(pfet_4)[1]/2) + pdk.util_max_metal_seperation())
+    # mp25_ref.movey(top_level.ymin - (2* evaluate_bbox(pfet_8)[1]/2) - 2* pdk.util_max_metal_seperation())
+
+    # ############################
+    # top_level.add(mp26_ref)
+    # top_level.add(mp25_ref)
+    # ############################
     top_level.add(mp19_ref)
     top_level.add(mp20_ref)
     top_level.add(mp18_ref)
@@ -239,6 +240,34 @@ def trimmer(
 
     top_level.add(mn5_ref)
     top_level.add(mn6_ref)
+
+
+        #############################
+
+    en_via = top_level << viam2m3
+    en_via.move(mp19_ref.ports["gate_W"].center).movex(-5)
+    top_level << straight_route(pdk, en_via.ports["bottom_met_E"],mp19_ref.ports["gate_W"],glayer1=tie_layers2[1], fullbottom=True )
+    top_level << c_route(pdk, en_via.ports["bottom_met_S"],mp20_ref.ports["gate_S"], e2glayer="met1", cglayer="met1",fullbottom=True )
+    
+    vdd_via1 = top_level << viam2m3
+    vdd_via1.move(mp19_ref.ports["source_N"].center).movey(10)
+    top_level << c_route(pdk, vdd_via1.ports["bottom_met_E"],mp19_ref.ports["source_E"], fullbottom=True )
+
+    vdd_via2 = top_level << viam2m3
+    vdd_via2.move(mp20_ref.ports["source_N"].center).movey(5)
+    top_level << c_route(pdk, vdd_via2.ports["bottom_met_E"],mp20_ref.ports["source_E"], fullbottom=True )
+    
+
+    top_level << c_route(pdk, mp19_ref.ports["drain_E"],mp1_2_ref.ports["port2_source_E"], fullbottom=True, extension=2*pdk.util_max_metal_seperation())
+    top_level << c_route(pdk, mp1_2_ref.ports["pfet_0_drain_W"],ncm12_ref.ports["fet_A_drain_W"], fullbottom=True )
+
+    top_level << c_route(pdk, ncm12_ref.ports["fet_A_gate_W"], mn5_ref.ports["drain_W"],fullbottom=True )
+    top_level << c_route(pdk, en_via.ports["bottom_met_W"], mn5_ref.ports["gate_W"],fullbottom=True )
+
+    top_level << c_route(pdk, mp20_ref.ports["drain_E"],mp3_ref.ports["source_E"], fullbottom=True)
+    
+
+    
 
    
     return top_level
